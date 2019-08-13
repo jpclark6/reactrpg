14845c0829d474bc3babeaa419edfd921c7ef405
Justin Clark <jpclark6@gmail.com>
2019-08-13 08:32:33 -0600

    Adds sign in and sign up pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
         <Switch>
-          <Route path='/' component={Dashboard} />
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
